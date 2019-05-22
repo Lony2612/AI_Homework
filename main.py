54537d5d3a155deae3c1dbc0438a4ec4a930c8b6
Lony2612 <1060653477@qq.com>
2019-05-22 09:39:08 +0800

    201905220939

--- main.py
+++ main.py
@@ -1,46 +1,180 @@
-from keras.models import Sequential, Model
+
 from keras.layers import *
-from keras.applications import *
-from keras.layers.advanced_activations import LeakyReLU
-from keras.activations import relu
-from keras.initializers import RandomNormal
-from keras.optimizers import RMSprop, SGD, Adam
-import keras.backend as K
+
+from utils import *
+from config import *
+from model import *
+
 import tensorflow as tf
-from tensorflow.contrib.distributions import Beta
-from random import shuffle
-from instance_normalization import InstanceNormalization
+import keras.backend as K
+from keras.optimizers import RMSprop, SGD, Adam
 import time
-from IPython.display import clear_output
-import matplotlib.pyplot as plt 
+
+import numpy as np
+from random import randint
 
 
+
+### Configuration
 K.set_learning_phase(1)
 
-channel_axis=-1 
-channel_first = False 
- 
-#nc_in = 9 # number of input channel 
-#nc_out = 4 # number of output channel 
- 
-# ========== Model config ========== 
-ngf = 64 
-ndf = 64 
-use_lsgan = False 
-位 = 10 if use_lsgan else 100 
-nc_G_inp = 9  
-nc_G_out = 4  
-nc_D_inp = 6  
-nc_D_out = 1  
-gamma_i = 0.1 
-use_instancenorm = True # False: batchnorm 
-use_mixup = True 
-linear_upsampling = False 
- 
-#========== Training config ========== 
-mixup_alpha = 0.1 
-imageSize = 256 
-batchSize = 8 
-lrD = 2e-4 
-lrG = 2e-4
+# ## Define models
+netGA = UNET_G(imageSize, nc_G_inp, nc_G_out, ngf)
+#netGA.summary()
 
+netDA = BASIC_D(nc_D_inp, ndf, use_sigmoid = not use_lsgan)
+#netDA.summary()
+
+
+# ## cycle_variables
+def cycle_variables(netG1):
+    """
+    Intermidiate params:
+        x_i: human w/ cloth i, shape=(128,96,3)
+        y_i: stand alone cloth i, shape=(128,96,3)
+        y_j: stand alone cloth j, shape=(128,96,3)
+        alpha: mask for x_i_j, shape=(128,96,1)
+        x_i_j: generated fake human swapping cloth i to j, shape=(128,96,3)
+    
+    Out:
+        real_input: concat[x_i, y_i, y_j], shape=(128,96,9)
+        fake_output: masked_x_i_j = alpha*x_i_j + (1-alpha)*x_i, shape=(128,96,3)
+        rec_input: output of the second generator (generating image similar to x_i), shape=(128,96,3)
+        fn_generate: a path from input to G_out and cyclic G_out
+    """
+    real_input = netG1.inputs[0]
+    fake_output = netG1.outputs[0]
+
+    # Legacy: how to split channels
+    # https://github.com/fchollet/keras/issues/5474
+    x_i = Lambda(lambda x: x[:,:,:, 0:3])(real_input)
+    y_i = Lambda(lambda x: x[:,:,:, 3:6])(real_input)
+    y_j = Lambda(lambda x: x[:,:,:, 6:])(real_input)
+    alpha = Lambda(lambda x: x[:,:,:, 0:1])(fake_output)
+    x_i_j = Lambda(lambda x: x[:,:,:, 1:])(fake_output)
+    
+    fake_output = alpha*x_i_j + (1-alpha)*x_i 
+    concat_input_G2 = concatenate([fake_output, y_j, y_i], axis=-1) # swap y_i and y_j
+    rec_input = netG1([concat_input_G2])
+    rec_alpha = Lambda(lambda x: x[:,:,:, 0:1])(rec_input)
+    rec_x_i_j = Lambda(lambda x: x[:,:,:, 1:])(rec_input)
+    rec_input = rec_alpha*rec_x_i_j + (1-rec_alpha)*fake_output
+    fn_generate = K.function([real_input], [fake_output, rec_input])
+    return real_input, fake_output, rec_input, fn_generate, alpha
+
+
+real_A, fake_B, rec_A, cycleA_generate, alpha_A = cycle_variables(netGA)
+
+
+# # Loss Function
+if use_lsgan:
+    loss_fn = lambda output, target : K.mean(K.abs(K.square(output-target)))
+else:
+    loss_fn = lambda output, target : -K.mean(K.log(output+1e-12)*target+K.log(1-output+1e-12)*(1-target))
+
+
+def D_loss(netD, real, fake, rec):
+    #x_i, y_i, y_j = tf.split(real, [3, 3, 3], 3)    
+    x_i = Lambda(lambda x: x[:,:,:, 0:3])(real)
+    y_i = Lambda(lambda x: x[:,:,:, 3:6])(real)
+    y_j = Lambda(lambda x: x[:,:,:, 6:])(real)
+    x_i_j = fake  
+    
+    output_real = netD(concatenate([x_i, y_i])) # positive sample
+    output_fake = netD(concatenate([x_i_j, y_j])) # negative sample
+    output_fake2 = netD(concatenate([x_i, y_j])) # negative sample 2
+    
+    loss_D_real = loss_fn(output_real, K.ones_like(output_real))    
+    loss_D_fake = loss_fn(output_fake, K.zeros_like(output_fake))
+    loss_D_fake2 = loss_fn(output_fake2, K.zeros_like(output_fake2)) # New loss term for discriminator    
+    if not use_nsgan:
+        loss_G = loss_fn(output_fake, K.ones_like(output_fake))
+    else:
+        loss_G = K.mean(K.log(output_fake))
+    
+    loss_D = loss_D_real+(loss_D_fake+loss_D_fake2)
+    loss_cyc = K.mean(K.abs(rec-x_i)) # cycle loss
+    return loss_D, loss_G, loss_cyc
+
+loss_DA, loss_GA, loss_cycA = D_loss(netDA, real_A, fake_B, rec_A)
+loss_cyc = loss_cycA
+loss_id = K.mean(K.abs(alpha_A)) # loss of alpha
+
+loss_G = loss_GA + 1*(1*loss_cyc + gamma_i*loss_id)
+loss_D = loss_DA*2
+
+weightsD = netDA.trainable_weights
+weightsG = netGA.trainable_weights
+
+training_updates = Adam(lr=lrD, beta_1=0.5).get_updates(weightsD,[],loss_D)
+netD_train = K.function([real_A],[loss_DA/2], training_updates)
+training_updates = Adam(lr=lrG, beta_1=0.5).get_updates(weightsG,[], loss_G)
+netG_train = K.function([real_A], [loss_GA, loss_cyc], training_updates)
+
+
+
+# # Load Image
+# 
+# Filenames:
+# 
+#     "./imgs/1/fileID_1.jpg" for human images.
+#     "./imgs/5/fileID_5.jpg" for article images.
+
+# Get filenames
+
+data = "imgs"
+train_A = load_data('./{}/1/*.jpg'.format(data))
+
+filenames_1 = load_data('./{}/1/*.jpg'.format(data))
+filenames_5 = load_data('./{}/5/*.jpg'.format(data))
+
+assert len(train_A)
+
+
+# ## Other utilities
+# # Training
+# 
+# Show results every 50 iterations.
+t0 = time.time()
+niter = 150
+gen_iterations = 0
+epoch = 0
+errCyc_sum = errGA_sum = errDA_sum = errC_sum = 0
+
+display_iters = 50
+train_batch = minibatchAB(train_A, batchSize, filenames_5)
+
+#while epoch < niter: 
+while gen_iterations < 5000:
+    epoch, A = next(train_batch)   
+    errDA  = netD_train([A])
+    errDA_sum +=errDA[0]
+
+    # epoch, trainA, trainB = next(train_batch)
+    errGA, errCyc = netG_train([A])
+    errGA_sum += errGA
+    errCyc_sum += errCyc
+    gen_iterations+=1
+    if gen_iterations%display_iters==0:
+        if gen_iterations%(10*display_iters)==0: # clear_output every 500 iters
+            clear_output()
+        print('[%d/%d][%d] Loss_D: %f Loss_G: %f loss_cyc: %f'
+        % (epoch, niter, gen_iterations, errDA_sum/display_iters,
+           errGA_sum/display_iters, errCyc_sum/display_iters), time.time()-t0)        
+        _, A = train_batch.send(4)
+        showG(cycleA_generate, A)        
+        errCyc_sum = errGA_sum = errDA_sum = errC_sum = 0
+
+# # Demo
+# 
+# Show 8 results on the same target article.
+len_fn = len(filenames_5)
+assert len_fn > 0
+idx = np.random.randint(len_fn)
+fn = filenames_5[idx]
+
+demo_batch = minibatchAB_demo(train_A, batchSize, fn)
+epoch, A = next(demo_batch) 
+
+_, A = demo_batch.send(8)
+showG(cycleA_generate, A)
---
+++ utils.py
@@ -0,0 +1,160 @@
+from random import randint, shuffle
+import numpy as np
+from PIL import Image
+import cv2 as cv2
+import glob
+
+from config import *
+
+def minibatch(data, batchsize, filenames_5):
+    length = len(data)
+    epoch = i = 0
+    tmpsize = None    
+    while True:
+        size = tmpsize if tmpsize else batchsize
+        if i+size > length:
+            shuffle(data)
+            i = 0
+            epoch+=1        
+        rtn = [read_image(data[j], filenames_5) for j in range(i,i+size)]
+        i+=size
+        tmpsize = yield epoch, np.float32(rtn)
+
+def minibatchAB(dataA, batchsize, filenames_5):
+    batchA=minibatch(dataA, batchsize, filenames_5)
+    tmpsize = None    
+    while True:        
+        ep1, A = batchA.send(tmpsize)
+        tmpsize = yield ep1, A
+
+def read_image(fn, filenames_5):
+    input_size = (111,148)
+    cropped_size = (96,128)
+    
+    if isRGB:
+    # Load human picture
+        im = Image.open(fn).convert('RGB')
+        im = im.resize( input_size, Image.BILINEAR )    
+    else:
+        im = cv2.imread(fn)
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2LAB)
+        im = cv2.resize(im, input_size, interpolation=cv2.INTER_CUBIC)
+    if apply_da is True:
+        im = crop_img(im, input_size, cropped_size)
+    arr = np.array(im)/255*2-1
+    img_x_i = arr
+    if channel_first:        
+        img_x_i = np.moveaxis(img_x_i, 2, 0)
+        
+    # Load article picture y_i
+    fn_y_i = fn[:-5] + "5.jpg"
+    fn_y_i = fn_y_i[:fn_y_i.rfind("/")-1] + "5/" + fn_y_i.split("/")[-1]
+    if isRGB:
+        im = Image.open(fn_y_i).convert('RGB')
+        im = im.resize(cropped_size, Image.BILINEAR )    
+    else:
+        im = cv2.imread(fn_y_i)
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2LAB)
+        im = cv2.resize(im, cropped_size, interpolation=cv2.INTER_CUBIC)
+    arr = np.array(im)/255*2-1
+    img_y_i = arr
+    if channel_first:        
+        img_y_i = np.moveaxis(img_y_i, 2, 0)
+    
+    # Load article picture y_j randomly
+    fn_y_j = np.random.choice(filenames_5)
+    while (fn_y_j == fn_y_i):
+        fn_y_j = np.random.choice(filenames_5)
+    if isRGB:
+        im = Image.open(fn_y_j).convert('RGB')
+        im = im.resize( cropped_size, Image.BILINEAR )
+    else:
+        im = cv2.imread(fn_y_j)
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2LAB)
+        im = cv2.resize(im, cropped_size, interpolation=cv2.INTER_CUBIC)
+    arr = np.array(im)/255*2-1
+    img_y_j = arr
+    if randint(0,1): 
+        img_y_j=img_y_j[:,::-1]
+    if channel_first:        
+        img_y_j = np.moveaxis(img_y_j, 2, 0)        
+    
+    if randint(0,1): # prevent disalign of the graphic on t-shirts and human when fplipping
+        img_x_i=img_x_i[:,::-1]
+        img_y_i=img_y_i[:,::-1]
+    
+    img = np.concatenate([img_x_i, img_y_i, img_y_j], axis=-1)    
+    assert img.shape[-1] == 9
+    
+    return img
+
+def load_data(file_pattern):
+    return glob.glob(file_pattern)
+
+def crop_img(img, large_size, small_size):
+    # only apply DA to human images
+    img_width = small_size[0]
+    img_height = small_size[1]
+    diff_size = (large_size[0]-small_size[0], large_size[1]-small_size[1])
+    
+    x_range = [i for i in range(diff_size[0])]
+    y_range = [j for j in range(diff_size[1])]
+    x0 = np.random.choice(x_range)
+    y0 = np.random.choice(y_range)
+    
+    img = np.array(img)
+    
+    img = img[y0: y0+img_height, x0: x0+img_width, :]
+    
+    return img
+
+def minibatch_demo(data, batchsize, fn_y_i=None):
+    length = len(data)
+    epoch = i = 0
+    tmpsize = None
+    shuffle(data)
+    while True:
+        size = tmpsize if tmpsize else batchsize
+        if i+size > length:
+            shuffle(data)
+            i = 0
+            epoch+=1    
+        rtn = [read_image(data[j], fn_y_i) for j in range(i,i+size)]
+        i+=size
+        tmpsize = yield epoch, np.float32(rtn)       
+
+def minibatchAB_demo(dataA, batchsize, fn_y_i=None):
+    batchA=minibatch_demo(dataA, batchsize, fn_y_i=fn_y_i)
+    tmpsize = None    
+    while True:        
+        ep1, A = batchA.send(tmpsize)
+        tmpsize = yield ep1, A
+
+from IPython.display import display
+def showX(X, rows=1):
+    assert X.shape[0]%rows == 0
+    int_X = ( (X+1)/2*255).clip(0,255).astype('uint8')
+    
+    if channel_first:
+        int_X = np.moveaxis(int_X.reshape(-1,3,128,96), 1, 3)
+    else:
+        if X.shape[-1] == 9:
+            img_x_i = int_X[:,:,:,:3]
+            img_y_i = int_X[:,:,:,3:6]
+            img_y_j = int_X[:,:,:,6:9]
+            int_X = np.concatenate([img_x_i, img_y_i, img_y_j], axis=1)
+        else:
+            int_X = int_X.reshape(-1,128,96, 3)
+    int_X = int_X.reshape(rows, -1, 128, 96,3).swapaxes(1,2).reshape(rows*imageSize,-1, 3)
+    if not isRGB:
+        int_X = cv2.cvtColor(int_X, cv2.COLOR_LAB2RGB)
+    display(Image.fromarray(int_X))
+
+
+def showG(cycleA_generate, A):
+    def G(fn_generate, X):
+        r = np.array([fn_generate([X[i:i+1]]) for i in range(X.shape[0])])
+        return r.swapaxes(0,1)[:,:,0]        
+    rA = G(cycleA_generate, A)
+    arr = np.concatenate([A[:,:,:,:3], A[:,:,:,3:6], A[:,:,:,6:9], rA[0], rA[1]])
+    showX(arr, 5) 
